ba32fe9ee8ed65b9576414110da9054726bc895f
Edward Smale <essmale2005@gmail.com>
2025-04-12 20:40:44 +0100

    Add help option to root ono command

--- src/main.zig
+++ src/main.zig
@@ -15,14 +15,22 @@ const help_screen = "ono <sub-command> [...options]\n\n" ++
         .{ "serve", "start a web server to view tasks, and rest api for managing them" },
     });
 
-const commands_map = .{
-    .{ "ls", @import("commands/ls.zig") },
-    // .{ "new", @import("commands/new.zig") },
-    // .{ "resolve", @import("commands/resolve.zig") },
-    // .{ "tag", @import("commands/tag.zig") },
-    // .{ "comment", @import("commands/comment.zig") },
-    // .{ "check", @import("commands/check.zig") },
-    // .{ "serve", @import("commands/serve.zig") },
+pub const Error = error{UnknownFlag};
+
+pub const Flag = enum {
+    init,
+    help,
+
+    pub fn fromShorthand(char: u8) ?Flag {
+        return switch (char) {
+            'h' => .help,
+            else => null,
+        };
+    }
+};
+
+pub const commands = struct {
+    pub const ls = @import("commands/ls.zig");
 };
 
 const use_gpa = switch (builtin.mode) {
@@ -47,22 +55,60 @@ pub fn main() !void {
     };
     _ = executable;
 
-    const sub_command = args_iterator.next() orelse {
+    var input_sub_command: ?[]const u8 = null;
+    defer if (input_sub_command) |sub_command| allocator.free(sub_command);
+    var input_show_help = false;
+
+    var full_shorthand: ?[]const u8 = null;
+    defer if (full_shorthand) |full| allocator.free(full);
+
+    var shorthand: []const u8 = "";
+    process_flag: switch (@as(Flag, .init)) {
+        .init => {
+            if (shorthand.len > 0) {
+                defer shorthand = shorthand[1..];
+                continue :process_flag Flag.fromShorthand(shorthand[0]) orelse return Error.UnknownFlag;
+            }
+            const next_arg = args_iterator.next() orelse break :process_flag;
+            if (next_arg[0] == '-') {
+                if (next_arg[1] == '-') {
+                    continue :process_flag std.meta.stringToEnum(Flag, next_arg[2..]) orelse
+                        return Error.UnknownFlag;
+                }
+                const duped_shorthand = try allocator.dupe(u8, next_arg);
+                if (full_shorthand) |full| allocator.free(full);
+                full_shorthand = duped_shorthand;
+                shorthand = duped_shorthand[1..];
+                continue :process_flag .init;
+            }
+
+            const duped = try allocator.dupe(u8, next_arg);
+            errdefer allocator.free(duped);
+            input_sub_command = duped;
+        },
+        .help => {
+            input_show_help = true;
+            continue :process_flag .init;
+        },
+    }
+
+    if (input_show_help) {
         try stdout_writer.print("{s}\n", .{help_screen});
-        return error.UnknownSubCommand;
-    };
+        return;
+    }
 
     const arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
 
-    inline for (commands_map) |entry| {
-        const sub_command_name, const sub_command_namespace = entry;
-        if (std.mem.eql(u8, sub_command, sub_command_name)) {
-            try sub_command_namespace.exec(allocator, &args_iterator, stdout_writer, stderr_writer);
-            break;
-        }
-    } else {
+    const sub_command = input_sub_command orelse {
         try stdout_writer.print("{s}\n", .{help_screen});
         return error.UnknownSubCommand;
+    };
+
+    inline for (@typeInfo(commands).@"struct".decls) |decl| {
+        if (std.mem.eql(u8, decl.name, sub_command)) {
+            try @field(commands, decl.name).exec(allocator, &args_iterator, stdout_writer, stderr_writer);
+            break;
+        }
     }
 }
